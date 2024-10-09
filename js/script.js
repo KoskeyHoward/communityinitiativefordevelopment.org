83080d684e8285d6a3abefa92ea8125e0de1e88c
Howard Koskey <koskeyhoward@gmail.com>
2024-10-09 20:15:57 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Redirection for Kenya
+document.getElementById('kenya-flag').onclick = function() {
+    window.location.href = 'https://cidokenya.org';
+};
+
+// Redirection for South Sudan
+document.getElementById('south-sudan-flag').onclick = function() {
+    window.location.href = 'https://cidosouthsudan.org';
+};
